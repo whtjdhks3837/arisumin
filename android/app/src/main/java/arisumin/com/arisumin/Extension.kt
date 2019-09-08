06b5e2f5b7eea50551ab68e305ffcc10c686eb2c
baejang <qowod77@naver.com>
2019-09-08 13:48:02 +0900

    공용 extenstion 추가

--- android/app/src/main/java/arisumin/com/arisumin/Extension.kt
+++ android/app/src/main/java/arisumin/com/arisumin/Extension.kt
@@ -1,1 +1,12 @@
 package arisumin.com.arisumin
+
+import android.app.Activity
+import android.content.Context
+import android.util.Log
+import android.util.TypedValue
+
+infix fun Activity.log(msg: String) = Log.i(this::class.java.simpleName, msg)
+
+fun toDp(context: Context, size: Float) = TypedValue.applyDimension(
+        TypedValue.COMPLEX_UNIT_DIP, size, context.resources.displayMetrics
+).toInt()
