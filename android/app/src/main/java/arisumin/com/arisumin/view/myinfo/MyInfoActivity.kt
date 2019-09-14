dbd936b5a13ee7a1306fe5eb47179b9012eb4f83
baejang <qowod77@naver.com>
2019-09-14 17:20:09 +0900

    my info 레이아웃 변경

--- android/app/src/main/java/arisumin/com/arisumin/view/myinfo/MyInfoActivity.kt
+++ android/app/src/main/java/arisumin/com/arisumin/view/myinfo/MyInfoActivity.kt
@@ -3,6 +3,7 @@ package arisumin.com.arisumin.view.myinfo
 import android.content.Context
 import android.os.Bundle
 import arisumin.com.arisumin.R
+import arisumin.com.arisumin.bindColor
 import arisumin.com.arisumin.databinding.ActivityMyInfoBinding
 import arisumin.com.arisumin.datasource.PREF_NAME
 import arisumin.com.arisumin.datasource.PreferenceModel
@@ -14,15 +15,21 @@ import arisumin.com.arisumin.view.main.MainActivity
 class MyInfoActivity : BaseActivity<ActivityMyInfoBinding>() {
 
     override val resourceId: Int = R.layout.activity_my_info
+    private val statusBarColor by bindColor(R.color.colorWhite)
 
     private val pref by lazy { MyInfoPref(this, PREF_NAME) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        if (pref.isSetting) {
+            startActivity<MainActivity>()
+        }
         super.onCreate(savedInstanceState)
+        window.statusBarColor = statusBarColor
         binding.confirm.setOnClickListener {
             if (validateInput()) {
                 pref.name = binding.inputName.text.toString()
                 pref.weight = binding.inputWeight.text.toString().toInt()
+                pref.isSetting = true
                 startActivity<MainActivity>()
             }
         }
@@ -44,4 +51,5 @@ class MyInfoActivity : BaseActivity<ActivityMyInfoBinding>() {
 private class MyInfoPref(context: Context, name: String) : PreferenceModel(context, name) {
     var name by stringPreference("name", null)
     var weight by intPreference("weight")
+    var isSetting by booleanPreference("isSetting")
 }
